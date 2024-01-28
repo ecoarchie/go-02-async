e6fd0235d5ae50b9e5e356dea96330f3fa2119de
ecoarchie <eco.artur@gmail.com>
2024-01-28 20:06:13 +0300

    refactor: remove comments, add helper functions

--- signer.go
+++ signer.go
@@ -8,6 +8,18 @@ import (
 	"sync"
 )
 
+func Crc32ToChan(data string, out chan<- string) {
+		out <- DataSignerCrc32(data)
+		close(out)
+}
+
+func Crc32FromMd5(s string, out chan<- string, mu *sync.Mutex) {
+		mu.Lock()
+		md5 := DataSignerMd5(s)
+		mu.Unlock()
+		Crc32ToChan(md5, out)
+}
+
 func SingleHash(in, out chan interface{}) {
 	wg := &sync.WaitGroup{}
 	mu := &sync.Mutex{}
@@ -17,20 +29,8 @@ func SingleHash(in, out chan interface{}) {
 		s := strconv.Itoa(i.(int))
 
 		wg.Add(1)
-		go func(s string, out chan<- string) {
-			mu.Lock()
-			md5 := DataSignerMd5(s)
-			mu.Unlock()
-			crsmd := DataSignerCrc32(md5)
-			out <- crsmd
-			close(out)
-		}(s, crsmd5chan)
-
-		go func(s string, out chan<- string) {
-			crs32 := DataSignerCrc32(s)
-			out <- crs32
-			close(out)
-		}(s, crs32chan)
+		go Crc32FromMd5(s, crsmd5chan, mu)
+		go Crc32ToChan(s, crs32chan)
 
 		go func() {
 			crsmd := <-crsmd5chan
@@ -43,6 +43,12 @@ func SingleHash(in, out chan interface{}) {
 	}
 	wg.Wait()
 	close(out)
+}
+
+func Crc32Th(inData interface{}, ch chan<- string, i int) {
+	s := strconv.Itoa(i)
+	ch <- DataSignerCrc32(s + inData.(string))
+	close(ch)
 }
 
 func MultiHash(in, out chan interface{}) {
@@ -60,10 +66,7 @@ func MultiHash(in, out chan interface{}) {
 			}
 
 			for i := 0; i < len(chans); i++ {
-				go func(inData interface{}, ch chan string, i int) {
-					s := strconv.Itoa(i)
-					ch <- DataSignerCrc32(s + inData.(string))
-				}(inData, chans[i], i)
+				go Crc32Th(inData, chans[i], i)
 			}
 			var step string
 			for _, ch := range chans {
@@ -105,61 +108,3 @@ func ExecutePipeline(jobs ...job) {
 		in = out
 	}
 }
-// MultiHash считает значение crc32(th+data)) (конкатенация цифры, приведённой к строке и строки),
-// где th=0..5 ( т.е. 6 хешей на каждое входящее значение ), потом берёт конкатенацию результатов в порядке расчета (0..5), где data - то что пришло на вход (и ушло на выход из SingleHash)
-// func MultiHash(in, out chan interface{}) {
-// 	fmt.Println("here in multilehash")
-// 	wg := &sync.WaitGroup{}
-// 	// var res string
-// 	for inData := range in {
-// 		wg.Add(1)
-// 		go func(inData interface{}) {
-// 			var step string
-// 			for i := 0; i < 6; i++ {
-// 				s := strconv.Itoa(i)
-// 				step += DataSignerCrc32(s + inData.(string))
-// 				fmt.Printf("multihash for step %d = %s\n", i, step)
-// 			}
-// 			fmt.Printf("res of Multihash = %s\n\n", step)
-// 			out <- step
-// 			wg.Done()
-// 		}(inData)
-// 	}
-// 	wg.Wait()
-// 	close(out)
-// }
-
-// func MultiHash(in, out chan interface{}) {
-// 	fmt.Println("here in multilehash")
-// 	wg := &sync.WaitGroup{}
-// 	// var res string
-// 	for inData := range in {
-// 		wg.Add(1)
-// 		go func(inData interface{}) {
-// 			var step string
-// 			output := make(chan string, 6)
-// 			wg2 := &sync.WaitGroup{}
-// 			wg2.Add(6)
-// 			for i := 0; i < 6; i++ {
-// 				go func(i int, output chan string) {
-// 					var step string
-// 					s := strconv.Itoa(i)
-// 					step += DataSignerCrc32(s + inData.(string))
-// 					fmt.Printf("multihash for step %d = %s\n", i, step)
-// 					output <- step
-// 					wg2.Done()
-// 				}(i, output)
-// 			}
-// 			wg2.Wait()
-// 			close(output)
-// 			for o := range output {
-// 				step += o
-// 			}
-// 			fmt.Printf("res of Multihash = %s\n\n", step)
-// 			out <- step
-// 			wg.Done()
-// 		}(inData)
-// 	}
-// 	wg.Wait()
-// 	close(out)
-// }
